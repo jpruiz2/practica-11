2095e272aa1a8fd18d5afda4987e6224fc1d732a
jpruiz2 <jpruizchavez@gmail.com>
2026-05-08 18:43:42 -0600

    Create ejercicio3.c

---
+++ ejercicio3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int resultado; // variable global
+
+void multiplicar(); // prototipo de la función
+
+int main()
+{
+    multiplicar(); // llamado de la función multiplicar
+    printf("%i", resultado);
+    return 0;
+}
+
+void multiplicar() // función multiplicar
+{
+    resultado = 5 * 4;
+}
